ee7dd8f25e3900ef3be8fd5e6e86636ecaf8859a
Albert <albert231207@mail.ru>
2024-12-18 22:25:59 +0300

    Initialize project

---
+++ ios-calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-calculator
+//
+//  Created by Albert on 12/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
